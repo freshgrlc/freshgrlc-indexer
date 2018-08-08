1122ff7f368f5a6f0ab6cdf183d30797491230e1
Erik van `t Wout <erik@ewnetworks.eu>
2018-08-08 21:06:56 +0200

    * Sample config

---
+++ config.sample.py
@@ -0,0 +1,7 @@
+
+class Configuration(object):
+    DAEMON_URL = 'http://indexerrpc:indexer@127.0.0.1:42070'
+    DATABASE_URL = 'mysql://indexer:indexer@localhost/garlicoin'
+
+    DEBUG_SQL = False
+

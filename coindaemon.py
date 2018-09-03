e1f8527d5e2128427b8b42f1d72d531a8a62df91
Erik van `t Wout <erik@ewnetworks.eu>
2018-09-03 21:04:49 +0200

    * Added helper function in coindaemon.Daemon for raw transaction signing

--- coindaemon.py
+++ coindaemon.py
@@ -1,5 +1,8 @@
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 
+class TransactionSignError(Exception):
+    pass
+
 class Daemon(AuthServiceProxy):
     def __init__(self, url):
         super(Daemon, self).__init__(url)
@@ -12,4 +15,11 @@ class Daemon(AuthServiceProxy):
 
     def load_transaction(self, txid):
         return self.decoderawtransaction(self.getrawtransaction(txid))
+
+    def sign_transaction(self, raw_tx_hex, private_keys):
+        result = self.signrawtransaction(raw_tx_hex, None, private_keys)
+        if result['complete']:
+            return result['hex']
+
+        raise TransactionSignError(result['errors'])
 
